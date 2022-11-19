96ed4075e56ea137d7fce430ffd54ba005f166f4
tusvel <tusvel@yandex.ru>
2022-11-19 20:01:01 +0300

    init commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require('sequelize');
+
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
